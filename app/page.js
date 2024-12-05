cdfccd3b1f4e3266f9fb5c491b2fa3e5e13416a8
Arismonx <arismonx@gmail.com>
2024-12-05 16:05:21 +0700

    Create page Login add Session Provider for NextAuth

--- app/page.js
+++ app/page.js
@@ -1,7 +1,58 @@
-export default function Home() {
+'use client'
+
+import { useState } from 'react'
+import { signIn } from 'next-auth/react'
+
+export default function SignIn() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      console.log('email: ',email)
+      console.log('passwprd: ',password)
+
+    } catch (error) {
+      console.log('error',error)
+    }
+  }
+
   return (
-    <div>
-      <div>hello World</div>
+    <div className="flex h-screen items-center justify-center">
+      <form
+        onSubmit={handleSubmit}
+        className="bg-white p-6 rounded-md shadow-md"
+      >
+        <div className="mb-4">
+          <label htmlFor="email">Email</label>
+          <input
+            id="email"
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+            className="w-full border border-gray-300 px-3 py-2 rounded" // Added border
+          />
+        </div>
+        <div className="mb-4">
+          <label htmlFor="password">Password</label>
+          <input
+            id="password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+            className="w-full border border-gray-300 px-3 py-2 rounded" // Added border
+          />
+        </div>
+        <button
+          type="submit"
+          className="w-full bg-blue-500 text-white py-2 rounded mb-4"
+        >
+          Sign In
+        </button>{' '}
+      </form>
     </div>
-  );
+  )
 }

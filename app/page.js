9b30a0bb5d084191f61102474aee687b07107dd0
Arismonx <arismonx@gmail.com>
2024-12-05 17:08:38 +0700

    add logout and callback login page if not yet login (from profile page)

--- app/page.js
+++ app/page.js
@@ -2,16 +2,27 @@
 
 import { useState } from 'react'
 import { signIn } from 'next-auth/react'
-
+import { useRouter } from 'next/navigation'
 export default function SignIn() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const router = useRouter()
 
   const handleSubmit = async (e) => {
     e.preventDefault()
     try {
-      console.log('email: ',email)
-      console.log('passwprd: ',password)
+      const result = await signIn('credentials',{
+        redirect: false,
+        email,
+        password
+      })
+
+      if( result.error ){
+        console.error(result.error)
+        return false
+      }
+
+      router.push('/profile')
 
     } catch (error) {
       console.log('error',error)

48dd497fb4ebb3757581bd5c86469098441d0493
Arismonx <arismonx@gmail.com>
2024-12-07 19:58:18 +0700

    add table account keep provider and token for social auth

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -8,61 +8,70 @@ import GoogleProvider from 'next-auth/providers/google'
 const prisma = new PrismaClient()
 
 export const authOptions = {
-    providers: [
-      CredentialsProvider({
-        name: 'Credentials',
-        credentials: {
-          email: { label: 'Email', type: 'email', placeholder: 'john@doe.com' },
-          password: { label: 'Password', type: 'password' },
-        },
-        async authorize(credentials, req) {
-          if (!credentials) return null
-          const user = await prisma.user.findUnique({
-            where: { email: credentials.email },
-          })
-  
-          if (
-            user &&
-            (await bcrypt.compare(credentials.password, user.password))
-          ) {
-            return {
-              id: user.id,
-              name: user.name,
-              email: user.email,
-              role: user.role
-            }
-          } else {
-            throw new Error('Invalid email or password')
-          }
-        },
-      }),
-      GoogleProvider({
-        clientId: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET
-      })
-    ],
-    adapter: PrismaAdapter(prisma),
-    session: {
-      strategy: 'jwt',
-    },
-    callbacks: {
-      jwt: async ({ token, user }) => {
-        if (user) {
-          token.id = user.id
-          token.role = user.role
-        }
-        return token
+  providers: [
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        email: { label: 'Email', type: 'email', placeholder: 'john@doe.com' },
+        password: { label: 'Password', type: 'password' },
       },
-      session: async ({ session, token }) => {
-        if (session.user) {
-          session.user.id = token.id
-          session.user.role = token.role
+      async authorize(credentials, req) {
+        if (!credentials) return null
+        const user = await prisma.user.findUnique({
+          where: { email: credentials.email },
+        })
+
+        if (
+          user &&
+          (await bcrypt.compare(credentials.password, user.password))
+        ) {
+          return {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            role: user.role
+          }
+        } else {
+          throw new Error('Invalid email or password')
         }
-        return session
+      },
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      profile(profile) {
+        return {
+          id: profile.sub,
+          name: `${profile.given_name} ${profile.family_name}`,
+          email: profile.email,
+          image: profile.picture,
+        }
       }
+    })
+  ],
+  adapter: PrismaAdapter(prisma),
+  session: {
+    strategy: 'jwt',
+  },
+  callbacks: {
+    jwt: async ({ token, user }) => {
+      if (user) {
+        token.id = user.id
+        token.role = user.role
+      }
+      return token
     },
-  }
-  
-  const handler = NextAuth(authOptions)
+    session: async ({ session, token }) => {
+      if (session.user) {
+        session.user.id = token.id
+        session.user.role = token.role
+        session.user.image = token.picture
+      }
+      return session
+    },
+  },
+}
+
+const handler = NextAuth(authOptions)
 
 export { handler as GET, handler as POST }
--- app/page.js
+++ app/page.js
@@ -3,6 +3,7 @@
 import { useState } from 'react'
 import { signIn } from 'next-auth/react'
 import { useRouter } from 'next/navigation'
+
 export default function SignIn() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
